6bd7e56f116f2f803e5186507fd435a34e6e1e94
Ratnesh <ratnesh@leftshift.io>
2016-06-15 12:14:09 +0530

    added a awesome component

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
+import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React this is a webpack! </p>;
+    return (
+      <div>
+        <p> Hello React this is a webpack! </p>
+        <AwesomeComponent />
+      </div>
+    )
   }
 }
 
--- src/client/public/bundle.js
+++ src/client/public/bundle.js
@@ -57,6 +57,10 @@
 	
 	var _reactDom = __webpack_require__(/*! react-dom */ 38);
 	
+	var _AwesomeComponent = __webpack_require__(/*! ./AwesomeComponent.jsx */ 168);
+	
+	var _AwesomeComponent2 = _interopRequireDefault(_AwesomeComponent);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -78,9 +82,14 @@
 	    key: 'render',
 	    value: function render() {
 	      return _react2.default.createElement(
-	        'p',
+	        'div',
 	        null,
-	        ' Hello React this is a webpack! '
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          ' Hello React this is a webpack! '
+	        ),
+	        _react2.default.createElement(_AwesomeComponent2.default, null)
 	      );
 	    }
 	  }]);
@@ -20883,6 +20892,83 @@
 	var ReactMount = __webpack_require__(/*! ./ReactMount */ 160);
 	
 	module.exports = ReactMount.renderSubtreeIntoContainer;
+
+/***/ },
+/* 168 */
+/*!*********************************************!*\
+  !*** ./src/client/app/AwesomeComponent.jsx ***!
+  \*********************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var AwesomeComponent = function (_React$Component) {
+	  _inherits(AwesomeComponent, _React$Component);
+	
+	  //state = { likesCount: 0 };
+	
+	  function AwesomeComponent(props) {
+	    _classCallCheck(this, AwesomeComponent);
+	
+	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(AwesomeComponent).call(this, props));
+	
+	    _this.state = { likesCount: 0 };
+	    return _this;
+	  }
+	
+	  _createClass(AwesomeComponent, [{
+	    key: 'onLike',
+	    value: function onLike() {
+	      var newLikesCount = this.state.likesCount + 1;
+	      this.setState({ likesCount: newLikesCount });
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        'Likes: ',
+	        _react2.default.createElement(
+	          'span',
+	          null,
+	          this.state.likesCount
+	        ),
+	        _react2.default.createElement(
+	          'div',
+	          null,
+	          _react2.default.createElement(
+	            'button',
+	            { onClick: this.onLike.bind(this) },
+	            'Like Me'
+	          )
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return AwesomeComponent;
+	}(_react2.default.Component);
+	
+	exports.default = AwesomeComponent;
 
 /***/ }
 /******/ ]);
